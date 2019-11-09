c6cefbfde331e8692d102adafb529ba0189c445a
Ed Cres <crespoed07@gmail.com>
2019-11-08 23:45:47 -0500

    added new activities

---
+++ app/src/main/java/com/aldreduser/belttools/MoreOptionsMenu.kt
@@ -0,0 +1,26 @@
+package com.aldreduser.belttools
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * TODO:
+ *
+ * make phone extensions editable by the user
+ * choose by departmetn button should be a drop-down menu
+ *
+ *  SKUs to work on: phone can scan barcode and save item code (hopefully read the sku too). Can keep notes of skus to work and what to do
+ *  Pallets to work on: phone can scan barcode and save pallet code. Can keep notes of pallets to work and what to do
+ *
+ * make the home screen contact the more options screen through the button
+ */
+
+class MoreOptionsMenu : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_more_options_menu)
+    }
+
+
+}
---
+++ app/src/main/java/com/aldreduser/belttools/ProDeskDept.kt
@@ -0,0 +1,12 @@
+package com.aldreduser.belttools
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ProDeskDept : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pro_desk_dept)
+    }
+}
