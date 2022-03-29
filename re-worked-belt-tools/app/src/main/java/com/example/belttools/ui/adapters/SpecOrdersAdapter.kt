8aed86508427eb720427e579b2a7a0b0cd4ab02c
Ed Cres <crespoed07@gmail.com>
2022-03-29 08:07:19 -0400

    Set up an itemClickListener for a recycler item in the adapter for specialty order, need to sent it to the View.

--- re-worked-belt-tools/app/src/main/java/com/example/belttools/ui/adapters/SpecOrdersAdapter.kt
+++ re-worked-belt-tools/app/src/main/java/com/example/belttools/ui/adapters/SpecOrdersAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.belttools.ui.adapters
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
@@ -11,43 +12,56 @@ import com.example.belttools.ui.viewmodel.SharedViewModel
 
 class SpecOrdersAdapter(
     private val viewModel: SharedViewModel,
+    private val onItemClickListener: OnItemClickListener
 ): ListAdapter<SpecialtyOrder, SpecOrdersAdapter.SpecOrdersViewHolder>(SpecOrderDiffCallback()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
-        SpecOrdersViewHolder.from(viewModel, parent)
+        SpecOrdersViewHolder.from(viewModel, onItemClickListener, parent)
 
     override fun onBindViewHolder(holderWorkouts: SpecOrdersViewHolder, position: Int) =
         holderWorkouts.bind(getItem(position))
 
     class SpecOrdersViewHolder private constructor(
         private val viewModel: SharedViewModel,
+        private val onItemClickListener: OnItemClickListener,
         private val binding: SpecialtyOrderItemBinding
-    ) : RecyclerView.ViewHolder(binding.root) {
+    ) : RecyclerView.ViewHolder(binding.root), View.OnClickListener {
+
+        init {
+            binding.orderRecyclerItemContainer.setOnClickListener(this)
+        }
 
         fun bind(specialtyOrder: SpecialtyOrder) {
             binding.apply {
                 orderIdTxt.text = specialtyOrder.id.toString()
                 orderNumberTxt.text = specialtyOrder.orderNum
                 orderNoteTxt.text = specialtyOrder.note
+            }
+        }
 
-                orderRecyclerItemContainer.setOnClickListener {
-                    // todo: display order in the view
-                    //  maybe do it through the the fragment and have an onClick interface
-                }
+        override fun onClick(view: View?) {
+            val position = adapterPosition
+            if (position != RecyclerView.NO_POSITION) {
+                onItemClickListener.onItemClick(position)
             }
         }
 
         companion object {
             fun from(
                 viewModel: SharedViewModel,
+                onItemClickListener: OnItemClickListener,
                 parent: ViewGroup
             ): SpecOrdersViewHolder {
                 val layoutInflater = LayoutInflater.from(parent.context)
                 val binding = SpecialtyOrderItemBinding
                     .inflate(layoutInflater, parent, false)
-                return SpecOrdersViewHolder(viewModel, binding)
+                return SpecOrdersViewHolder(viewModel, onItemClickListener, binding)
             }
         }
+    }
+
+    interface OnItemClickListener {
+        fun onItemClick(position: Int)
     }
 }
 
